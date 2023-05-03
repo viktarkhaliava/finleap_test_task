e9a0f732aac3944952826119b8d1f0e307d9b3c9
viktarkhaliava <viktar.khaliava@gmail.com>
2023-05-03 13:15:43 +0200

    feat: env vars, packages configs & mocks

---
+++ src/mocks/index.ts
@@ -0,0 +1,9 @@
+import * as monzo from './monzo-tx.json';
+import * as revolut from './revolut-tx.json';
+import * as sterling_bank from './sterling-tx.json';
+
+export {
+  monzo,
+  revolut,
+  sterling_bank,
+}
